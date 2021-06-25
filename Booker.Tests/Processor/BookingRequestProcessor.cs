d4e6f380188b5b14a17465185234b940c8e67882
Dominik Wikliński <domel2222@gmail.com>
2021-06-25 09:02:53 +0200

    created new class BookingRequestProcessor

---
+++ Booker.Tests/Processor/BookingRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Booker.Tests.Processor
+{
+    internal class BookingRequestProcessor
+    {
+        public BookingRequestProcessor()
+        {
+        }
+
+        internal BookingResult BookEvent(BookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
