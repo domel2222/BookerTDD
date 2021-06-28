e9c83d4b20bf86fa8a17f56e75f9c9933bcb8118
Dominik Wikliński <domel2222@gmail.com>
2021-06-28 10:43:51 +0200

    Event added props Description

--- Booker.DataAccess/EventBookerContext.cs
+++ Booker.DataAccess/EventBookerContext.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using Booker.Modals;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Booker.DataAccess
 {
-    public class EventBookerContext
+    public class EventBookerContext : DbContext
     {
+        public EventBookerContext(DbContextOptions<EventBookerContext> options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedData(modelBuilder);
+        }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().HasData(
+                
+                new Event {Id = 1,  })
+        }
     }
 }
--- Booker/Modals/Event.cs
+++ Booker/Modals/Event.cs
@@ -3,5 +3,6 @@
     public class Event
     {
         public int Id { get; set; }
+        public string Description { get; set; }
     }
 }
