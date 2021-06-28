b8a044a9953fb887d3e4cccfb996f78f36b9dd33
Dominik Wikliński <domel2222@gmail.com>
2021-06-28 09:12:56 +0200

    created projects Data access and tests for DB moreover created validation folder and tests

---
+++ Booker.Tests/Processor/Validation/DateInFutureAttributeTests.cs
@@ -0,0 +1,19 @@
+﻿using Booker.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker.Tests.Processor.Validation
+{
+    public class DateInFutureAttributeTests
+    {
+        public void ShouldReturndatamustBeInTheFuture(bool expectedIsValid, int hour, int minute, int second)
+        {
+            var dateTime = new DateTime(2021, 6, 28, hour, minute, second);
+
+            var attribute = new DateWithoutTimeAttribute();
+        }
+    }
+}
