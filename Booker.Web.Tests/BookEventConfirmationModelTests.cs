65d18a826f743ae4ebb90466b1260a5c9304f6bb
Dominik Wikliński <domel2222@gmail.com>
2021-06-30 08:27:01 +0200

    created razor BookEventConfirmationModel
    in addition wrote test:
    BookEventConfirmationModelTests >
     StoreParameretValuesInProperties()

---
+++ Booker.Web.Tests/BookEventConfirmationModelTests.cs
@@ -0,0 +1,34 @@
+﻿using Booker.Web.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace Booker.Web.Tests
+{
+    public class BookEventConfirmationModelTests
+    {
+        [Fact]
+        public void StoreParameretValuesInProperties()
+        {
+            //arrange
+            const int eventBookingid = 13;
+            const string firstName = "Marek";
+            var date = new DateTime(2021, 8, 1);
+
+            var bookEventConfirmationModel = new BookEventConfirmationModel();
+
+            //act
+            bookEventConfirmationModel.OnGet(eventBookingid, firstName, date);
+
+            //assert
+
+            eventBookingid.ShouldBe(bookEventConfirmationModel.EventBookingId);
+            firstName.ShouldBe(bookEventConfirmationModel.FirstName);
+            date.ShouldBe(bookEventConfirmationModel.Date);
+        }
+    }
+}
---
+++ Booker.Web/Pages/BookEventConfirmation.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booker.Web.Pages
+{
+    public class BookEventConfirmationModel : PageModel
+    {
+        public DateTime Date { get; set; }
+        public string FirstName { get; set; }
+        public int EventBookingId { get; set; }
+
+        public void OnGet(int eventBookingid, string firstName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
