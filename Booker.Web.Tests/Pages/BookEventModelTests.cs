7ca31af568d1d0add6fcab6546f53576202a498d
Dominik Wikliński <domel2222@gmail.com>
2021-07-01 07:05:08 +0200

    created two tests BookEventModelTests : CallBookEventMethodOfProcesor_Mock CallBookEventMethodOfProcesor_NSubstitute

--- Booker.Web.Tests/Pages/BookEventModelTests.cs
+++ Booker.Web.Tests/Pages/BookEventModelTests.cs
@@ -5,14 +5,49 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Shouldly;
+using Booker.Processor;
+using Moq;
+using Booker.Web.Pages;
+using Booker.Modals;
+using NSubstitute;
 
 namespace Booker.Web.Tests
 {
     public class BookEventModelTests
     {
         [Fact]
-        public void CallBookEventMethodOfProcesor()
+        public void CallBookEventMethodOfProcesor_Mock()
         {
+            //arrange 
+
+            var processorMock = new Mock<IEventBookingRequestProcessor>();
+
+            var bookEventModel = new BookEventModel(processorMock.Object)
+            {
+                EventBookingRequest = new EventBookingRequest()
+            };
+
+            //act
+            bookEventModel.OnPost();
+            //assert
+            processorMock.Verify(x => x.BookEvent(bookEventModel.EventBookingRequest), Times.Once);
+        }
+        [Fact]
+
+        public void CallBookEventMethodOfProcesor_NSubstitute()
+        {
+            //arrange
+            var procesorNSub = Substitute.For<IEventBookingRequestProcessor>();
+
+            var bookEventModel = new BookEventModel(procesorNSub)
+            {
+                EventBookingRequest = new EventBookingRequest()
+            };
+            //act
+
+            bookEventModel.OnPost();
+            //assert
+            procesorNSub.Received().BookEvent(bookEventModel.EventBookingRequest);
 
         }
     }
--- Booker.Web/Pages/BookEvent.cshtml.cs
+++ Booker.Web/Pages/BookEvent.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Booker.Modals;
+using Booker.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,10 +11,18 @@ namespace Booker.Web.Pages
 {
     public class BookEventModel : PageModel
     {
+        private IEventBookingRequestProcessor _eventBookingRequestProcessor;
+
+        public BookEventModel(IEventBookingRequestProcessor eventBookingRequestProcessor)
+        {
+            this._eventBookingRequestProcessor = eventBookingRequestProcessor;
+        }
+
         [BindProperty]
         public EventBookingRequest EventBookingRequest { get; set; }
         public void OnPost()
         {
+            _eventBookingRequestProcessor.BookEvent(EventBookingRequest);
         }
     }
 }
